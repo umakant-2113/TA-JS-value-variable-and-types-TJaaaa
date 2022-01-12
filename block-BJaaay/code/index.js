5b8102e021647715c8df25b8d77c89b25c7cd90d
umakant-2113 <lodhiumakant800@gmail.com>
2022-01-12 17:45:26 +0530

    assignment 6

--- block-BJaaay/code/index.js
+++ block-BJaaay/code/index.js
@@ -11,3 +11,26 @@
 // Calculate the sum of all numbers from 1 to 10.
 
 // Log all the values from 1 to 10 using while loop
+for (let i = 0; i <= 10; i = i + 1) {
+  console.log(i);
+}
+
+for (let i = 0; i <= 10; i = i + 2) {
+  console.log(i);
+}
+for (let i = 1; i <= 10; i = i + 2) {
+  console.log(i);
+}
+let total = 0;
+let num = 10;
+for (i = 1; i <= 10; i++) {
+  total = total + i;
+}
+console.log(total);
+
+
+let i = 0;
+while (i <= 10) {
+  console.log(i);
+  i++;
+}
